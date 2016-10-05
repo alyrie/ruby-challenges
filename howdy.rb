2f5f68a28fb54f497c8c9967f54202dca80dc0a8
divgal <alyrie@yahoo.com>
2016-10-05 18:43:58 -0500

    Here's my code for announcing a winner

---
+++ howdy.rb
@@ -0,0 +1,15 @@
+#my hash info
+
+
+project_01 = { name: "Fruit Loop Dash", author: "Mickey Mouse"}
+project_02 = { name: "Cut the Rope", author: "Donald Duck"}
+project_03 = { name: "Lucky Charm Run", author: "Daffy Duck"}
+project_04 = { name: "Angry Ducks", author: "Daisy Duck"}
+project_05 = { name: "Hoop Madness", author: "Sylvester"}
+project_06 = { name: "Lean Tower", author: "Ruby Rail"}
+
+
+winner = project_03[:name]
+
+
+puts "#{winner} wins the award for the most unique game!"
